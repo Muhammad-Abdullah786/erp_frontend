09d2333bf6e1a9012a11616be01eb44568165519
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-16 19:37:07 +0500

    fixed url

--- src/components/Tracking_search/Tracking_Search.tsx
+++ src/components/Tracking_search/Tracking_Search.tsx
@@ -1,36 +1,36 @@
-"use client"
-import React, { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import useStore from '@/store/Zustand_Store'
-import { useRouter } from 'next/navigation'
-import axios from 'axios'
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import useStore from "@/store/Zustand_Store";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { url } from "@/apiURL";
 const Tracking_Search = () => {
-
   const router = useRouter();
-  const set_tracking_id = useStore((state) => state.set_tracking_id)
+  const set_tracking_id = useStore((state) => state.set_tracking_id);
   const [search, setsearch] = useState("");
   const [show, setshow] = useState(false);
-  
+
   const handlechange = (e: any) => {
     const value = e.target.value;
     setsearch(value);
     setshow(value.trim() === ""); // Update `show` dynamically based on input
   };
-  
+
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     if (!search.trim()) {
       setshow(true); // Show red border if the input is empty
       return;
     }
-    console.log(search)
+    console.log(search);
     try {
-      const response = await axios.post(`http://localhost:3000/v1/container/tracking_container`, {
+      const response = await axios.post(`${url}/container/tracking_container`, {
         tracking_id: search,
       });
       if (response.status !== 200) {
-        console.log(response)
+        console.log(response);
         setshow(true);
         return;
       }
@@ -39,24 +39,30 @@ const Tracking_Search = () => {
       setsearch("");
       router.push(`/tracking`);
     } catch (error) {
-        console.log(error)
+      console.log(error);
       setshow(true); // Show red border on error
       setsearch("");
     }
   };
 
   return (
-    <div className='min-h-screen'>
-     <form onSubmit={handleSubmit} >
-      <div className="h-[450px] flex justify-center items-center ">
-          <div className='flex w-full max-w-md items-center space-x-2'>
-            <Input className={show ? "border-red-600 border-2" : ""} onChange={(e) => handlechange(e)} value={search}  type="text" placeholder="Enter Tracking ID" />
-            <Button type="submit" >Track Now</Button>
+    <div className="min-h-screen">
+      <form onSubmit={handleSubmit}>
+        <div className="h-[450px] flex justify-center items-center ">
+          <div className="flex w-full max-w-md items-center space-x-2">
+            <Input
+              className={show ? "border-red-600 border-2" : ""}
+              onChange={(e) => handlechange(e)}
+              value={search}
+              type="text"
+              placeholder="Enter Tracking ID"
+            />
+            <Button type="submit">Track Now</Button>
           </div>
-      </div>
+        </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Tracking_Search;
