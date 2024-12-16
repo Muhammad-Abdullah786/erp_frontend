23f1bfccb135a927ba871271afb2cb74bd2bdcdb
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-16 20:51:37 +0500

    final code

--- src/apiURL.ts
+++ src/apiURL.ts
@@ -1,3 +1,2 @@
-// export const url = "https://back-mu-olive.vercel.app/v1";
-export const url = "https://erp-gold-phi.vercel.app/v1";
+export const url = "https://back-mu-olive.vercel.app/v1";
 // export const url = "http://localhost:3000/v1";
