df13199e8113609bbbd9757e48294cbe537285fd
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-16 21:05:44 +0500

    changed url

--- src/apiURL.ts
+++ src/apiURL.ts
@@ -1,2 +1,4 @@
-export const url = "https://back-mu-olive.vercel.app/v1";
+// export const url = "https://back-mu-olive.vercel.app/v1"; // for main
+export const url =
+  "https://erp-frontend-git-dev-muhammad-abdullah786s-projects.vercel.app/v1"; // this is for devevlop branch
 // export const url = "http://localhost:3000/v1";
