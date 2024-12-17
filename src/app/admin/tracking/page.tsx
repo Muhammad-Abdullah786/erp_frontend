6b9f507476776c17cb7d467d8a952c88ecf36c88
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-17 22:39:08 +0500

    changed admin trackign

--- src/app/admin/tracking/page.tsx
+++ src/app/admin/tracking/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import Admin_Tracking from "@/components/AdminTracking/Admin_Tracking";
+// import Admin_Tracking from "@/components/AdminTracking/Admin_Tracking";
 // Dynamically import the AdminTracking component with SSR disabled
-// const AdminTrackingWithNoSSR = dynamic(
-//   () => import("@/components/AdminTracking/Admin_Tracking"),
-//   {
-//     ssr: false, // Disable SSR for the AdminTracking component
-//   }
-// );
+const Admin_Tracking = dynamic(
+  () => import("@/components/AdminTracking/Admin_Tracking"),
+  {
+    ssr: false, // Disable SSR for the AdminTracking component
+  }
+);
 
 const Page = () => {
   return <Admin_Tracking />;
