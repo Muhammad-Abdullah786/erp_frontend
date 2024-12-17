3c462c196430e7ccc2e2c63294218044cf77d94a
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-17 22:46:07 +0500

    fixed front

--- src/apiURL.ts
+++ src/apiURL.ts
@@ -1,6 +1,6 @@
-// export const url = "https://erp-gold-phi.vercel.app/v1";
+export const url = "https://erp-gold-phi.vercel.app/v1";
 // export const url = "https://back-mu-olive.vercel.app/v1"; // for main
 // export const url =
 //   "https://erp-git-dev-muhammad-abdullah786s-projects.vercel.app/v1";
 // // this is for devevlop branch
-export const url = "http://localhost:3000/v1";
+// export const url = "http://localhost:3000/v1";
